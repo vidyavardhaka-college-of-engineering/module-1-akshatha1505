8b639deaba491ea7d972780237ee0a244c60e108
akshatha1505 <84765349+akshatha1505@users.noreply.github.com>
2021-06-24 15:36:38 +0530

    done

--- Program-8.c
+++ Program-8.c
@@ -7,10 +7,10 @@ int main()
     int x, y;
     printf(" Enter the value of x: ");
     scanf("%d",&x);
-    printf("y = %d  ",y=++x);
-    printf("y = %d  ",y=x++);
-    printf("y = %d  ",y=--x);
-    printf("y = %d  ",y=x--); 
+    printf("y = %d ",y=++x);
+    printf("y = %d ",y=x++);
+    printf("y = %d ",y=--x);
+    printf("y = %d",y=x--); 
     return 0;
 }
 
